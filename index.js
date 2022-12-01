e1f826b0ed75dca11723cde96db3c96e5a61ca64
tej00001 <tejachintu207@gmail.com>
2022-12-01 22:09:24 +0530

    addingindex.html and index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+//console.dir(document)
+
+//console.log(document.visibilityState)
+
+//console.log(document.hidden)
+
+var items= document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+
+
+
+items[2].style.backgroundColor = "green";
+
+for(let i=0;i<items.length;i++)
+{
+items[i].style.fontWeight = "bold";
+
+}
